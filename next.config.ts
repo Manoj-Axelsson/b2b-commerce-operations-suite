0534a2bd5e14c2572c688887594820b24b9b4d53
Manoj Axelsson <mr.mjaxelsson@gmail.com>
2026-03-05 10:06:22 +0100

    chore/fix-config-conventions or similar.
    chore: align project config with DX conventions.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  outputFileTracingRoot: path.join(import.meta.dirname),
+
+  turbopack: {
+    root: path.join(import.meta.dirname),
+  },
 };
 
 export default nextConfig;
