50fcfee890eeb23e3b3afa37bfec84ebdbbe2fcb
Manoj Axelsson <mr.mjaxelsson@gmail.com>
2026-03-06 19:29:26 +0100

    chore(home): refactor images to static imports for preload optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import nextLogo from "../../public/next.svg";
+import vercelLogo from "../../public/vercel.svg";
 
 export default function Home() {
   return (
@@ -6,7 +8,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={nextLogo}
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,7 +45,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={vercelLogo}
               alt="Vercel logomark"
               width={16}
               height={16}
@@ -51,7 +53,7 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
