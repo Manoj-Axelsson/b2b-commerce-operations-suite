2caa7377d196b4aba9c9e6ac4ed1d52526618634
Manoj Axelsson <mr.mjaxelsson@gmail.com>
2026-03-19 13:55:27 +0100

    chore: apply EB Garamond globally and update UI dependencies

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+type HeaderProps = {
+    className?: string;
+};
+
+export const Header = ({ className }: HeaderProps) => {
+    return (
+        <header className={`flex justify-center items-center ${className}`}>
+            <Image
+                src="/rajput_logo.svg"
+                alt="Rajput Foods Logo"
+                width={400}
+                height={400}
+                priority
+                className="w-[400px] h-auto object-contain"
+            />
+        </header>
+    );
+};
